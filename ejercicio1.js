77c92436197fee20c19e179615d774dc7e7efc44
DiegoPalacios1221 <yeyoPRO8@gmail.com>
2024-04-04 18:57:56 -0500

    Corregido

--- ejercicio1.js
+++ ejercicio1.js
@@ -6,10 +6,10 @@ calificacion = prompt("Ingrese un valor de 1 a 5 para la calificacion correctas 
 asistencias = prompt("Ingrese del 1 al 5 las asistencias del estudiante");
 
 if (asistencias > 4) {
-    alert("El estudiante" + estudiante) ("ha aprobado");
+    alert("El estudiante " + estudiante + " ha aprobado");
     
 }
   
 else {
-    alert("El estudiante" + estudiante) ("ha reprobado");
+    alert("El estudiante " +  estudiante  + " ha reprobado");
 }
